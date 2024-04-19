7fd1eba934cb1a4137a00924392b670a6d60a303
normuradoff <normuradoff1@gmail.com>
2024-04-19 12:34:15 +0500

    create packages

---
+++ packages/common/vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsConfigPaths(), dts({ include: ['src/components'] })],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'Components',
+      formats: ['es', 'umd'],
+      fileName: (format) => `components.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+});
