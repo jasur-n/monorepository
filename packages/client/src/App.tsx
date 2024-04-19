2f4e95cce9f407cf55b5de496107c5fc0904d33d
normuradoff <normuradoff1@gmail.com>
2024-04-19 16:46:06 +0500

    configure packages to reuse components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Header } from '@npm-workspaces/common';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Header text="Hello, world from common header!" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
