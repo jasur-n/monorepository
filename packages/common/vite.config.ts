8e3939723f0c40316b1fa7e786d8fd0dc5532188
normuradoff <normuradoff1@gmail.com>
2024-04-20 18:55:07 +0500

    update packages versions

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -1,13 +1,19 @@
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import * as packageJson from './package.json';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths(), dts({ include: ['src/components'] })],
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ['src/components/'],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve('src', 'components/index.ts'),
@@ -19,4 +25,4 @@ export default defineConfig({
       external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
-});
+}));
